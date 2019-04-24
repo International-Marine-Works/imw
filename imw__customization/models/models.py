babc08a3ff54713f6624f55cfe3b865a8f004017
International-Marine-Works <47365350+International-Marine-Works@users.noreply.github.com>
2019-04-24 14:19:50 +0200

    Update models.py

--- imw__customization/models/models.py
+++ imw__customization/models/models.py
@@ -1,17 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from odoo import api, fields, models, _
- 
+from odoo import api, fields, models
 
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
-
-    imw_qty = fields.Float(string='Quantity')
-    imw_measurement = fields.Float(string='Measurement',default=1)
-    otherUnitMeasure = fields.Many2one('uom.uom', 'Other Unit of Measure')
- 
- 
 class imw_product_template(models.Model):
     #_name = 'product.template'
     _inherit = 'product.template'
