6eec3cc32f7e65fef9fbf690b97c4c39f9ea59b0
International-Marine-Works <47365350+International-Marine-Works@users.noreply.github.com>
2019-04-24 19:31:58 +0200

    Update models.py

--- imw__customization/models/models.py
+++ imw__customization/models/models.py
@@ -7,9 +7,6 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     imw_qty = fields.Float(string='Quantity')
-    imw_measurement = fields.Float(string='Measurement',default=1)
-    category_id = fields.Many2one('product.category', 'category')
-    otherUnitMeasure = fields.Many2one('uom.uom', 'Other Unit of Measure')
  
 class imw_product_template(models.Model):
     #_name = 'product.template'
