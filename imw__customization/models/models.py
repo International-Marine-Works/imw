2ee4335fb67d1be5f14b4ecf6eabe44803eda3d2
International-Marine-Works <47365350+International-Marine-Works@users.noreply.github.com>
2019-04-24 15:15:34 +0200

    Update models.py

--- imw__customization/models/models.py
+++ imw__customization/models/models.py
@@ -29,38 +29,6 @@ class SaleOrderLine(models.Model):
 
 
 
-
-  # @api.model
-  # def _selection_filter(self):
-  #     """ Get the list of filter allowed according to the options checked
-  #     in 'Settings\Warehouse'. """
-  #     records.filtered(lambda r: r.company_id == user.company_id)
-  #     res_filter = [
-  #         ('none', _('All products')),
-  #         ('category', _('One product category')),
-  #         ('product', _('One product only')),
-  #         ('partial', _('Select products manually'))]
-
-  #     if self.user_has_groups('stock.group_tracking_owner'):
-  #         res_filter += [('owner', _('One owner only')), ('product_owner', _('One product for a specific owner'))]
-  #     if self.user_has_groups('stock.group_production_lot'):
-  #         res_filter.append(('lot', _('One Lot/Serial Number')))
-  #     if self.user_has_groups('stock.group_tracking_lot'):
-  #         res_filter.append(('pack', _('A Pack')))
-  #     return res_filter
-
-  # @api.onchange('filter')
-  # def onchange_filter(self):
-  #     if self.filter not in ('product', 'product_owner'):
-  #         self.product_id = False
-  #     if self.filter not in ('owner', 'product_owner'):
-  #         self.package_id = False
-  #     if self.filter != 'category':
-  #         self.category_id = False
-  #     if self.filter == 'product':
-  #         self.exhausted = True
-
-
     @api.onchange('product_id')
     def onchange_product(self):
         res = {}
@@ -76,29 +44,6 @@ class imw_product_template(models.Model):
     #_name = 'product.template'
     _inherit = 'product.template'
     otherUnitMeasure = fields.Many2one('uom.uom', 'Other Unit of Measure')
-
-
-
-
-
-
-class PurchaseOrderLine(models.Model):
-    _inherit = 'purchase.order.line'
-
-    imw_qty = fields.Float(string='Quantity')
-    imw_measurement = fields.Float(string='Measurement',default=1)
-    category_id = fields.Many2one('product.category', 'category')
-    otherUnitMeasure = fields.Many2one('uom.uom', 'Other Unit of Measure')
-
-    @api.multi
-    @api.onchange('imw_qty')
-    def _value_pc(self):
-        self.product_qty = float(self.imw_qty) * float(self.imw_measurement)
-
-
-
-
-
 
 
 
