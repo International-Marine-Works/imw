8770953e9fbf717b020aa0bcae01bfb94e01a32f
International-Marine-Works <47365350+International-Marine-Works@users.noreply.github.com>
2019-04-24 20:21:30 +0200

    Update __manifest__.py

--- imw__customization/__manifest__.py
+++ imw__customization/__manifest__.py
@@ -22,7 +22,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','Sales','Inventory','Purchase'],
+    'depends': ['base','sale','inventory','purchase'],
 
     # always loaded
     'data': [
