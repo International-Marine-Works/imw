05b79472102c59b57275b069b1b157fe58f0a062
International-Marine-Works <47365350+International-Marine-Works@users.noreply.github.com>
2019-04-25 18:00:21 +0200

    Update models.py

--- imw__customization/models/models.py
+++ imw__customization/models/models.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api 
- 
+
+class imw_product_template(models.Model):
+    #_name = 'product.template'
+    _inherit = 'product.template'
+    otherUnitMeasure = fields.Many2one('uom.uom', 'Other Unit of Measure')
