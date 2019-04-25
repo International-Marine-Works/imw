fa2b5a7d20d3f0e71d6c438ec064bc177131ca34
International-Marine-Works <47365350+International-Marine-Works@users.noreply.github.com>
2019-04-26 00:34:36 +0200

    Update models.py

--- imw__customization/models/models.py
+++ imw__customization/models/models.py
@@ -55,3 +55,36 @@ class AccountBankStatement(models.Model):
     referenceRecive = fields.Char(string='R Reference', states={'open': [('readonly', False)]}, copy=False,
                            readonly=True,
                           help="Used to hold the reference of the external mean that created this statement (name of imported file, reference of online synchronization...)")
+
+    
+    
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    imw_qty = fields.Float(string='Quantity')
+    imw_measurement = fields.Float(string='Measurement',default=1)
+    category_id = fields.Many2one('product.category', 'category')
+    otherUnitMeasure = fields.Many2one('uom.uom', 'Other Unit of Measure')
+
+    @api.multi
+    @api.onchange('imw_qty', 'imw_measurement')
+    def _ChangeQty(self):
+         if float(self.imw_measurement) == 0:
+            self.imw_measurement = 1
+
+         self.quantity = float(self.imw_qty) * float(self.imw_measurement)
+
+
+    @api.multi
+    @api.onchange('product_id')
+    def _onchangeProductId(self):
+        self.otherUnitMeasure = self.product_id.otherUnitMeasure
+
+        if float(self.imw_qty) == 0:
+            self.imw_qty = 1
+
+        if float(self.imw_measurement) == 0:
+            self.imw_measurement = 1
+            # self.product_uom_qty = float(self.imw_qty) * float(self.imw_measurement)
+        imwQty = float(self.imw_qty) if float(self.imw_qty) > 0 else 1
+        self.imw_measurement = float(self.quantity) / imwQty
