7defe25c9cd9e0e88f1f93b39a1e5fd10b5d2172
International-Marine-Works <47365350+International-Marine-Works@users.noreply.github.com>
2019-04-26 15:09:04 +0200

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from . import account
+#from . import account
 from . import product
-from . import sale
-from . import purchase
+#from . import sale
+#from . import purchase
 #from . import models
