f78e5481cf6f9dd3f1ebb3d42c5e70b2d584f6bf
International-Marine-Works <47365350+International-Marine-Works@users.noreply.github.com>
2019-04-26 14:37:16 +0200

    Add files via upload

--- imw__customization/models/__init__.py
+++ imw__customization/models/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#from . import product
-#from . import sale
-#from . import account
-#from . import purchase
-from . import models
+from . import product
+from . import sale
+from . import account
+from . import purchase
--- imw__customization/models/purchase.py
+++ imw__customization/models/purchase.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo import api, fields, models, _
+from odoo.addons import decimal_precision as dp
+from odoo.exceptions import UserError
+from odoo.tools import float_utils
 import logging
 _logger = logging.getLogger(__name__)
  
