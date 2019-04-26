4bc6c49cbe53c69253770837561a128f78ce99bd
International-Marine-Works <47365350+International-Marine-Works@users.noreply.github.com>
2019-04-26 11:26:06 +0200

    Add files via upload

---
+++ imw__customization/models/product.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo import api, fields, models, _
+from odoo.addons import decimal_precision as dp
+from odoo.exceptions import UserError
+from odoo.tools import float_utils
+import logging
+_logger = logging.getLogger(__name__)
+ 
+class imw_product_template(models.Model):
+    _name = 'product.template'
+    _inherit = 'product.template'
+    otherUnitMeasure = fields.Many2one('uom.uom', 'Other Unit of Measure')
+
+ 
+
+
