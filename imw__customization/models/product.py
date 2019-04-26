1f93ffde9f47fca853a5c70a208cb47b1ba4fc32
International-Marine-Works <ahmed.nour@sky.com.eg>
2019-04-26 16:58:30 +0200

    last 4_58

--- imw__customization/models/product.py
+++ imw__customization/models/product.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+import logging
+
+_logger = logging.getLogger(__name__)
+ 
 
 class imw_product_template(models.Model):
     _name = 'product.template'
